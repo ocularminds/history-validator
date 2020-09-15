65277572285fb52e858670ed2fda41221ad7fa72
Festus B. Jejelowo <mail.festus@gmail.com>
2020-09-15 21:00:12 +0100

    Dev02 (#11)
    * Added Stored Procedure call to statement search.
    
    * Fix stored procedure implementation
    
    * Fixed autosize fit for excel and amount formatting
    
    * added user access priviledges and error management
    
    * add repository persitence for transaction history
    
    * added JPA configuration
    
    * Fixed id generation for entity
    
    * Enabled sql log

--- src/main/java/com/leadway/ps/Loader.java
+++ src/main/java/com/leadway/ps/Loader.java
@@ -4,9 +4,13 @@ import com.leadway.ps.model.Department;
 import com.leadway.ps.model.Resource;
 import com.leadway.ps.model.User;
 import com.leadway.ps.service.UserService;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,20 +18,18 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class Loader implements ApplicationRunner {
-  private UserService users;
+  private final UserService users;
 
   @Autowired
   public Loader(UserService userService) {
     this.users = userService;
   }
 
-  public void run(ApplicationArguments args) {
+  @Override
+  public void run(ApplicationArguments aa) throws Exception {
     if (this.users.findAll().isEmpty()) {
-      System.out.println("--- Populating user records ----");
+      System.out.println("--- Populating  records ----");
       System.out.println("Please note that this should come from database");
-      User user;
-      Department department;
-      Resource resource;
       String[][] depts = {
         { "001", "Asset Management" },
         { "002", "Benefit Processing" },
@@ -44,7 +46,9 @@ public class Loader implements ApplicationRunner {
         { "103", "Operations" },
         { "104", "Risk Management & Strategy" },
       };
+      System.out.println("--- 1. Populating department records ----");
       for (String[] d : depts) {
+        System.out.println("--- \t"+Arrays.toString(d));
         this.users.add(new Department(d[0], d[1]));
       }
       String[][] res = {
@@ -54,15 +58,19 @@ public class Loader implements ApplicationRunner {
         { "Statements", "statements/reviews", "Review", "REVIEWER" },
         { "Statements", "statements/approvals", "Approvals", "AUTHORIZER" },
       };
+      System.out.println("--- 2. Populating resources[menu] records ----");
       for (String[] r : res) {
+        System.out.println("--- \t"+Arrays.toString(r));
         this.users.add(new Resource(r[0], r[1], r[2], r[3]));
       }
+      System.out.println("--- 3. Populating user records ----");
       String[][] appusers = {
         { "n-yusuf", "Nurudeen", "Yusuf", "INITIATOR", "100" },
-        { "a-ayodeji", "Adeotun", "Owoyemi", "REVIEWER", "005" },
-        { "a-omogbe", "Adeotun", "Omogbemi", "AUTHORIZER", "101" },
+        { "o-shewu", "Oluwatosin", "Tosin", "REVIEWER", "005" },
+        { "o-aliu", "Olayinka", "Aliu", "AUTHORIZER", "101" },
       };
       for (String[] u : appusers) {
+        System.out.println("--- \t"+Arrays.toString(u));
         this.users.add(new User(u[0], u[1], u[2], u[3], u[4]));
       }
       System.out.println(
