1195e49d1943be215141df8fe0f3fe292f2c7227
Festus B. Jejelowo <mail.festus@gmail.com>
2020-09-16 01:28:48 +0100

    Dev02 (#12)
    * Added Stored Procedure call to statement search.
    
    * Fix stored procedure implementation
    
    * Fixed autosize fit for excel and amount formatting
    
    * added user access priviledges and error management
    
    * add repository persitence for transaction history
    
    * added JPA configuration
    
    * Fixed id generation for entity
    
    * Enabled sql log
    
    * add transaction management and admin module

--- src/main/java/com/leadway/ps/model/Department.java
+++ src/main/java/com/leadway/ps/model/Department.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "th_departments")
-public class Department {
+public class Department implements java.io.Serializable{
   
   @Id
   @Column(name = "code", nullable = false, length = 22)
--- src/main/java/com/leadway/ps/model/User.java
+++ src/main/java/com/leadway/ps/model/User.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "th_users")
-public class User {
+public class User implements java.io.Serializable{
   @Id
   @Column(name = "userid", nullable = false, length = 22)
   private String id;
