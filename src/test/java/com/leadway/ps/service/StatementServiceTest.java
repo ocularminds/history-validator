939999f0dd02355255fdede0f96e9b394395ff9d
Festus B. Jejelowo <mail.festus@gmail.com>
2020-09-18 17:09:05 +0100

    Added Tests

---
+++ src/test/java/com/leadway/ps/service/StatementServiceTest.java
@@ -0,0 +1,12 @@
+package com.leadway.ps.service;
+
+import org.junit.*;
+
+public class StatementServiceTest{
+    @Test 
+    public void testCreate(){}
+
+    @Test
+    public void testSearch(){}
+
+}
