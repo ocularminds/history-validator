049d959cd4726e21118292bf535842ee1e15f73b
Festus B. Jejelowo <mail.festus@gmail.com>
2020-09-18 15:43:28 +0100

    added search build query

---
+++ src/main/java/com/leadway/ps/QueryBuilder.java
@@ -0,0 +1,169 @@
+package com.leadway.ps;
+
+public class QueryBuilder {
+
+  public static String search() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("select e.firstname,e.lastname,e.MiddleName,e.rsapin,e.fundid, ");
+    sb.append("re.employercode, fd.FundName from employee join employer er ");
+    sb.append("on e.employerid = er.employerid join  funddefinition fd ");
+    sb.append("on fd.funddefinitionid = e.fundid and e.rsapin = ?");
+    return sb.toString();
+  }
+
+  public static String build() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("select e.rsapin,c.Employeeid, Valuedate [PAY_RECEIVE_DATE],  ");
+    sb.append(
+      "StartPeriod [RELATED_MONTH_START], EndPeriod [RELATED_MONTH_END],  "
+    );
+    sb.append(
+      "[TRANSACTION_TYPE]=case when cast(ContributionTypeID as varchar)  "
+    );
+    sb.append("in (11,12,13,14,66) then 'AR001'  ");
+    sb.append("when cast(ContributionTypeID as varchar) in (3) then 'VLC01'  ");
+    sb.append(
+      "when cast(ContributionTypeID as varchar) in (17,18,32) then 'OST01'  "
+    );
+    sb.append("when cast(ContributionTypeID as varchar) in (71) then	'NR001' ");
+    sb.append("when cast(ContributionTypeID as varchar) in  ");
+    sb.append(
+      "(1,10,29,41,43,44,46,49,51,53,54,55,56,57,60,62,63,67,68,69,70,72,74,39,59,75) then	'MC001' "
+    );
+    sb.append("when cast(ContributionTypeID as varchar) in (31) then	'NST01'  ");
+    sb.append(
+      "when cast(ContributionTypeID as varchar) in (2,34) then	'LEG01' "
+    );
+    sb.append(
+      "when cast(ContributionTypeID as varchar) in (73,15,19,20) then 'RFD01'  "
+    );
+    sb.append(
+      "when (cast(ContributionTypeID as varchar) in (9) AND c.Fundid=9) then 'TFD01'  "
+    );
+    sb.append(
+      "when (cast(ContributionTypeID as varchar) in (37) AND c.Fundid=9) then 'TFD01' "
+    );
+    sb.append(
+      "when (cast(ContributionTypeID as varchar) in (9) AND c.Fundid=1) then 'TFD02'  "
+    );
+    sb.append(
+      "when (cast(ContributionTypeID as varchar) in (37) AND c.Fundid=1) then 'TFD02'  "
+    );
+    sb.append(
+      "when (cast(ContributionTypeID as varchar) in (9) AND c.Fundid=10) then 'TFD03'  "
+    );
+    sb.append(
+      "when (cast(ContributionTypeID as varchar) in (37) AND c.Fundid=10) then 'TFD03'  "
+    );
+    sb.append(
+      "when (cast(ContributionTypeID as varchar) in (6,7,9,36,45) AND c.Fundid=2) then 'TFD04' "
+    );
+    sb.append(
+      "when (cast(ContributionTypeID as varchar) in (9) AND c.Fundid=13) then 'TFD05' "
+    );
+    sb.append(
+      "when (cast(ContributionTypeID as varchar) in (8) AND c.Fundid=9) then 'TFD07' "
+    );
+    sb.append(
+      "when (cast(ContributionTypeID as varchar) in (8) AND c.Fundid=1) then 'TFD08' "
+    );
+    sb.append(
+      "when (cast(ContributionTypeID as varchar) in (8) AND c.Fundid=10) then 'TFD09' "
+    );
+    sb.append(
+      "when (cast(ContributionTypeID as varchar) in (8) AND c.Fundid=2) then 'TFD10' "
+    );
+    sb.append(
+      "when (cast(ContributionTypeID as varchar) in (8) AND c.Fundid=13) then 'TFD11' "
+    );
+    sb.append(" else cast(ContributionTypeID as varchar) END ");
+    sb.append(
+      ",case when cast(ContributionTypeID as varchar) in (11,12,13,14,66)  "
+    );
+    sb.append("then 0.00 else EmployerAmount end [EMPLOYER_CONTRIBUTION], ");
+    sb.append(
+      "case when cast(ContributionTypeID as varchar) in (11,12,13,14,66)  "
+    );
+    sb.append("then 0.00 else EmployeeAmount end [EMPLOYEE_CONTRIBUTION],  ");
+    sb.append(
+      "AVCAmount [VOLUNTARY_CONTINGENT],  0.00 [VOLUNTARY_RETIREMENT],  "
+    );
+    sb.append(
+      "[OTHER_INFLOWS]=case when cast(ContributionTypeID as varchar) in (11,12,13,14,66)  "
+    );
+    sb.append("then EmployerAmount+EmployeeAmount  ");
+    sb.append(
+      "else EmployeeAccumAmount+EmployeeLegacyAmount+EmployerAccumAmount+EmployerLegacyAmount "
+    );
+    sb.append(
+      "end, GrossAmount [TOTAL_CONTRIBUTIONS], UnitValue [NUMBER_OF_UNITS], "
+    );
+    sb.append("AdminFee [FEES],0.00 [OTHER_WITHDRAWALS], ");
+    sb.append(
+      "NET_CONTRIBUTIONS = ISNULL(GrossAmount,0.00)- 0.00, '0023' [RELATED_PFA_CODE] "
+    );
+    sb.append(
+      "from contributions c join employee e on e.employeeid = c.EmployeeID "
+    );
+    sb.append(
+      "where isnull(c.isConfirmed,0) = 1 and isnull(c.isarchived,0)=0 "
+    );
+    sb.append("and e.rsapin in (?) ");
+    sb.append("UNION ");
+    sb.append("select rsapin, p.Employeeid, ValueDate [PAY_RECEIVE_DATE], ");
+    sb.append(
+      "StartPeriod [RELATED_MONTH_START], EndPeriod [RELATED_MONTH_END], "
+    );
+    sb.append(
+      " [TRANSACTION_TYPE]=case when cast(PaymentTypeID as varchar) in (1,22,78) then 'LS001'  "
+    );
+    sb.append(
+      "when cast(PaymentTypeID as varchar) in (3,8,17,33,34,35,36) then 'PW001'  "
+    );
+    sb.append("when cast(PaymentTypeID as varchar) in (7) then 'WT001' ");
+    sb.append("when cast(PaymentTypeID as varchar) in (9) then 'PT025'  ");
+    sb.append("when cast(PaymentTypeID as varchar) in (10) then 'NST01'  ");
+    sb.append("when cast(PaymentTypeID as varchar) in (13,19) then 'LEG01' ");
+    sb.append(
+      "when (cast(PaymentTypeID as varchar) in (14) AND p.Fundid=9) then 'TFD01' "
+    );
+    sb.append(
+      "when (cast(PaymentTypeID as varchar) in (14) AND p.Fundid=1) then 'TFD02'  "
+    );
+    sb.append(
+      "when (cast(PaymentTypeID as varchar) in (14) AND p.Fundid=10) then 'TFD03' "
+    );
+    sb.append(
+      "when (cast(PaymentTypeID as varchar) in (14,87) AND p.Fundid=2) then 'TFD04' "
+    );
+    sb.append(
+      "when (cast(PaymentTypeID as varchar) in (14) AND p.Fundid=13) then 'TFD05' "
+    );
+    sb.append(
+      "when cast(PaymentTypeID as varchar) in (15,50,69) then 'EXC01'  "
+    );
+    sb.append(
+      "when cast(PaymentTypeID as varchar) in (20,48,66) then 'CREF1'  "
+    );
+    sb.append("when cast(PaymentTypeID as varchar) in (29,49) then 'RFD01' ");
+    sb.append(
+      "when cast(PaymentTypeID as varchar) in (26,38,40,41,42,57,85) then 'TRF02'  "
+    );
+    sb.append("when cast(PaymentTypeID as varchar) in  ");
+    sb.append("(12,25, 43,44,46,52,53,54,55,58,61,63,68,70,79) then 'REV01'  ");
+    sb.append("when cast(PaymentTypeID as varchar) in (86) then 'MPP02' ");
+    sb.append("when cast(PaymentTypeID as varchar) in (67,88) then 'EXMP1' ");
+    sb.append("else cast(PaymentTypeID as varchar) end, ");
+    sb.append("0.00, 0.00, 0.00, 0.00, 0.00, 0.00, ");
+    sb.append("UnitValue [NUMBER_OF_UNITS], Fee [FEES] , ");
+    sb.append("ISNULL(Gross,0.00) [OTHER_WITHDRAWALS],");
+    sb.append("NET_CONTRIBUTIONS = 0.00 - ISNULL(Gross,0.00) , ");
+    sb.append("'0023' [RELATED_PFA_CODE]  ");
+    sb.append("From payments p join employee e ");
+    sb.append("on p.employeeid = e.employeeid ");
+    sb.append("where isnull(isConfirmed,0) = 1 and isnull(isarchived,0)=0 ");
+    sb.append("and e.rsapin in (?) ");
+    sb.append("order by 1, 2, 3 ");
+    return sb.toString();
+  }
+}
--- src/main/java/com/leadway/ps/service/StatementService.java
+++ src/main/java/com/leadway/ps/service/StatementService.java
@@ -1,6 +1,7 @@
 package com.leadway.ps.service;
 
 import com.leadway.ps.ExcelFile;
+import com.leadway.ps.QueryBuilder;
 import com.leadway.ps.RecordRowMapper;
 import com.leadway.ps.StatementRowMapper;
 import com.leadway.ps.model.Approval;
@@ -59,49 +60,37 @@ public class StatementService {
   }
 
   public StatementRequest search(String pin) {
-    StringBuilder sb = new StringBuilder();
-    sb.append("select e.firstname,e.lastname,e.MiddleName,e.rsapin,e.fundid, ");
-    sb.append("re.employercode, fd.FundName from employee join employer er ");
-    sb.append("on e.employerid = er.employerid join  funddefinition fd ");
-    sb.append("on fd.funddefinitionid = e.fundid and e.rsapin = ?");
     List<StatementRequest> list = template.query(
-      sb.toString(),
+      QueryBuilder.search(),
       new Object[] { pin },
       new StatementRowMapper()
     );
     return list.size() > 0 ? list.get(0) : null;
   }
 
-  public List<StatementRequest> create(Criteria criteria, String user) {
-    List<StatementRequest> searched = randomise(criteria);
-    requests.addAll(searched);
-    return searched;
-  }
-
   public List<StatementRequest> search(Criteria criteria, String user) {
     List<StatementRequest> data = new ArrayList<>();
+    if (
+      criteria.getPin() == null || criteria.getPin().trim().isEmpty()
+    ) return data;
     StatementRequest req = search(criteria.getPin());
     if (req == null) {
       return data;
     }
     req.setRequester(user);
-    SqlParameterSource in = new MapSqlParameterSource()
-      .addValue("FundID", req.getFundId())
-      .addValue("RSAPIN", criteria.getPin())
-      .addValue("FromDate", criteria.getFrom())
-      .addValue("ToDate", criteria.getTo());
-
     try {
-      Map<String, Object> out = procedure.execute(in);
-      if (out == null) {
-        data.add(req);
+      List<Record> records = template.query(
+        QueryBuilder.build(),
+        new Object[] { criteria.getPin(), criteria.getPin() },
+        new RecordRowMapper()
+      );
+
+      if (records == null || records.isEmpty()) {
         return data;
       }
-      List<Record> records = (List<Record>) out.get("statements");
-      addCalculatedValues(req, records);
+      req.setRecords(records);
       req = repository.save(req);
       data.add(req);
-      //requests.addAll(data);
     } catch (Exception e) {
       e.printStackTrace();
       System.err.println(e.getMessage());
@@ -129,124 +118,5 @@ public class StatementService {
     req.setStatus(approval.getApproval().name());
     req.getComments().add(approval.getComment());
     repository.save(req);
-  }
-
-  public List<StatementRequest> randomise(Criteria criteria) {
-    int random;
-    int rand = (int) (Math.random() * 25);
-    long time = new Date().getTime();
-    List<StatementRequest> data = new ArrayList<>();
-    List<Record> records;
-    Record record;
-    String[] names = {
-      "Akpan, Jinadu, Paul",
-      "Segun,Hammed, Bello",
-      "Chukwu,Festus,Adimeru",
-      "Omosetan,Omorele,Pius",
-      "Djeri,Oriola,Yakub",
-    };
-    BigDecimal total, units, fundUnits, net, netSum;
-    for (int i = 0; i < rand; i++) {
-      random = (int) (Math.random() * 5);
-      String[] emp = names[random].split(",");
-      StatementRequest req = new StatementRequest();
-      req.setFirstName(emp[0]);
-      req.setSurname(emp[1]);
-      req.setMiddleName(emp[2]);
-      req.setPin(criteria.getPin());
-      req.setEmployer("PR0000070199");
-      req.setCode("FUND3");
-      req.setPrice(new BigDecimal("3.7230"));
-      netSum = BigDecimal.ZERO;
-      fundUnits =
-        new BigDecimal(Math.random() * 99999999)
-        .setScale(2, RoundingMode.HALF_UP);
-      req.setUnits(fundUnits);
-      req.setBalance(fundUnits.multiply(req.getPrice()));
-      req.setStatus("PENDING");
-      int max = (int) (Math.random() * 25);
-      long amount = (int) (Math.random() * 99999999l);
-      BigDecimal debits = new BigDecimal((long) (Math.random() * 76543210l));
-      records = new ArrayList<>();
-      for (int j = 0; j < max; j++) {
-        int t = (int) (Math.random() * 1000 * 60 * 60 * 24 * 90);
-        record = new Record();
-        record.setDateReceived(new Date(time - t));
-        record.setMonthStart(new Date(time - t - (1000 * 60 * 60 * 24 * 30)));
-        record.setMonthEnd(new Date(time - t - (1000 * 60 * 60 * 24 * 5)));
-        record.setType("TF003");
-        record.setEmployer(
-          new BigDecimal(amount * 0.44).setScale(2, RoundingMode.HALF_UP)
-        );
-        record.setContribution(
-          new BigDecimal(amount * 0.56).setScale(2, RoundingMode.HALF_UP)
-        );
-        record.setVoluntaryContigent(BigDecimal.ZERO);
-        record.setVoluntaryRetirement(BigDecimal.ZERO);
-        record.setOtherInflows(BigDecimal.ZERO);
-        total = record.getEmployer().add(record.getContribution());
-        total = total.add(record.getVoluntaryContigent());
-        total = total.add(record.getVoluntaryRetirement());
-        total = total.add(record.getOtherInflows());
-        units = total.divide(req.getPrice(), 2, RoundingMode.HALF_UP);
-        record.setTotal(total);
-        record.setUnits(units);
-        record.setFees(new BigDecimal("100.00"));
-        record.setWithdrawals(debits);
-        net = total.subtract(record.getFees()).subtract(debits);
-        netSum = netSum.add(net);
-        record.setNet(net);
-        record.setPfa("022");
-        records.add(record);
-      }
-
-      req.setEarning(req.getBalance().subtract(netSum));
-      req.setRecords(records);
-      data.add(req);
-    }
-    return data;
-  }
-
-  private void addCalculatedValues(StatementRequest req, List<Record> data) {
-    req.setPrice(new BigDecimal("3.7230"));
-    BigDecimal netSum = BigDecimal.ZERO, totalSum = BigDecimal.ZERO, unitSum =
-      BigDecimal.ZERO;
-    int max = data.size();
-    BigDecimal debits, total, net;
-    List<Record> records = new ArrayList<>();
-    Record record;
-    for (int j = 0; j < max; j++) {
-      record = data.get(j);
-      record.setVoluntaryContigent(BigDecimal.ZERO);
-      record.setVoluntaryRetirement(BigDecimal.ZERO);
-      record.setOtherInflows(BigDecimal.ZERO);
-      total = record.getEmployer().add(record.getContribution());
-      total = total.add(record.getVoluntaryContigent());
-      total = total.add(record.getVoluntaryRetirement());
-      total = total.add(record.getOtherInflows());
-      unitSum = unitSum.add(req.getUnits());
-      debits = record.getWithdrawals();
-      record.setTotal(total);
-      net = total.subtract(record.getFees()).subtract(debits);
-      netSum = netSum.add(net);
-      totalSum = totalSum.add(total);
-      record.setNet(net);
-      record.setId(req.getPin() + record.getPfa() + j);
-      records.add(record);
-    }
-
-    req.setUnits(unitSum);
-    req.setBalance(
-      unitSum.multiply(req.getPrice()).setScale(2, RoundingMode.HALF_UP)
-    );
-    req.setEarning(req.getBalance().subtract(netSum));
-    req.setRecords(records);
-  }
-
-  public static void main(String[] args) throws Exception {
-    Criteria criteria = new Criteria(200, "PEN100016997603");
-    StatementService service = new StatementService(null, null, null);
-    List<StatementRequest> req = service.randomise(criteria);
-    new ExcelFile(req.get(0)).toFile();
   }
 }
