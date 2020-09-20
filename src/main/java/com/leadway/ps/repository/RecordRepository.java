93213d216ed65a2e7bd0a2c16c9a01290d1b1031
Festus B. Jejelowo <mail.festus@gmail.com>
2020-09-20 15:00:44 +0100

    Features (#34)
    * added number formatter
    
    * added number formatter
    
    * added number formatter
    
    * added number formatter
    
    * added number formatter
    
    * clean up
    
    * added number formatter
    
    * added number formatter
    
    * added number formatter
    
    * improving code coverage
    
    * Added Login service test

--- src/main/java/com/leadway/ps/repository/RecordRepository.java
+++ src/main/java/com/leadway/ps/repository/RecordRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RecordRepository extends JpaRepository<Record, String>{
-    List<Record> findAllByRequest(Statement request);
+    List<Record> findAllByStatement(Statement statement);
 }
---
+++ src/test/java/com/leadway/ps/service/LoginServiceTest.java
@@ -0,0 +1,54 @@
+package com.leadway.ps.service;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.leadway.ps.model.Credentials;
+import com.leadway.ps.model.User;
+import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LoginServiceTest {
+  @Mock
+  UserService users;
+
+  @InjectMocks
+  LoginService service;
+
+  @Test
+  public void testLogin() throws Exception {
+    Credentials credentials = new Credentials();
+    credentials.setUsername("3244");
+    credentials.setPassword("pass");
+    when(users.get(any(String.class))).thenReturn(new User());
+    boolean result = service.authenticate(credentials);
+    assertTrue(result);
+  }
+
+  @Test
+  public void testLoginFailedWhenPasswordIsNotMatch() throws Exception {
+    Credentials credentials = new Credentials();
+    credentials.setUsername("3244");
+    credentials.setPassword("olodo");
+    when(users.get(any(String.class))).thenReturn(new User());
+    boolean result = service.authenticate(credentials);
+    assertTrue(!result);
+  }
+
+  @Test
+  public void testLoginFailedWhenUserNameAndPasswordIsNull() throws Exception {
+    Credentials credentials = new Credentials();
+    boolean result = service.authenticate(credentials);
+    assertTrue(!result);
+  }
+}
